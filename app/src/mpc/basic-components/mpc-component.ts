16e58ed5eacf42bb2edcb5b6290044cdcf307fd3
Daniel Bendel <daniel.bendel@gmail.com>
2016-12-29 19:19:54 +0100

    connect package and version chooser, implement "add pair" and enable the button if both are selected

--- app/src/mpc/basic-components/mpc-component.ts
+++ app/src/mpc/basic-components/mpc-component.ts
@@ -1,3 +1,26 @@
-export class MpcComponent {
+import {computedFrom} from 'aurelia-framework';
 
+export class MpcComponent {
+  public selectedPackage: string = null;
+  public selectedVersion: string = null;
+
+  @computedFrom('selectedPackage', 'selectedVersion')
+  public get isAddPairButtonEnabled() {
+    return this.selectedPackage !== null && this.selectedVersion !== null;
+  }
+
+  public onPackageSelect(selection) {
+    this.selectedPackage = selection.dataItem;
+  }
+
+  public onVersionSelect(e) {
+    let grid = e.sender;
+    let selectedRow = grid.select();
+    let dataItem: string = grid.dataItem(selectedRow).ver;
+    this.selectedVersion = dataItem;
+  }
+
+  public addThisPair() {
+    alert(`add: ${this.selectedPackage}@${this.selectedVersion}`);
+  }
 }
