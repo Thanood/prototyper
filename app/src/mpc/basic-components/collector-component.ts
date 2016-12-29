3a73471f13180a8a275b20bfb5cecdaf3e588811
Daniel Bendel <daniel.bendel@gmail.com>
2016-12-29 21:21:51 +0100

    remove test data, display placeholder when selected pairs are empty

--- app/src/mpc/basic-components/collector-component.ts
+++ app/src/mpc/basic-components/collector-component.ts
@@ -1,46 +1,23 @@
 import {bindable} from 'aurelia-framework';
 
 export class CollectorComponent {
+  public packages = [];
   public scrollable = { virtual: true };
 
-  private testData = [];
-
-  // tslint:disable-next-line:no-unused-variable
-  private datasource = new kendo.data.DataSource({
-    transport: {
-      read: (options) => {
-
-        // here you can do an async API call to get your data
-        // or use a local array with data
-        // when you have all the data that you need you can call options.success()
-
-        // the options variable contains information about pagination, filtering etc so
-        // that can be used when doing an query
-
-        options.success(this.testData);
-      }
-    }
-  });
-
-  constructor() {
-    this.testData = [
-      { fModule: 'aurelia-kendoui-bridge', fVersion: '1.0.1' },
-      { fModule: 'aurelia-materialize-bridge', fVersion: '0.1.2' }
-    ];
-  };
+  private datasource = new kendo.data.DataSource();
 
   public addPackage(module: string, version: string) {
-    // this.testData.push({ module: name, fVersion: version });
-    // this.datasource.data(this.testData);
     this.datasource.pushCreate({ fModule: module, fVersion: version });
+    this.packages.push({ fModule: module, fVersion: version });
   }
 
   public removeSelectedPackages() {
     const d = this.datasource.data();
     d.forEach(item => {
-      if (item.fSelected) {
-        this.datasource.remove(item);
+      if ((item as any).fSelected) {
+        this.datasource.remove((item as any));
       }
     });
+    this.packages = (this.datasource.data() as any);
   }
 }
