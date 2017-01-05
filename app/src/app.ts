ec4a4c675e97498c2b0e9ddd1db50e653e084b80
Nikolaj Ivancic <nik@congral.com>
2017-01-05 10:29:26 -0500

    add flex-box

--- app/src/app.ts
+++ app/src/app.ts
@@ -7,10 +7,9 @@ export class App {
     config.title = 'Monterey prototyper';
     config.map([
       { route: ['', 'welcome'], name: 'welcome',      moduleId: 'welcome/screen',       nav: true, title: 'Welcome' },
+      { route: ['flex-box'],    name: 'flex-box',     moduleId: 'flex-box/screen',      nav: true, title: 'Flex box' },      
       { route: ['mpc'],         name: 'mpc',      	  moduleId: 'mpc/screen',           nav: true, title: 'MPC' },
       { route: ['atc'],         name: 'atc',      	  moduleId: 'atc/screen',           nav: true, title: 'ATC' },
-
-
     ]);
 
     this.router = router;
